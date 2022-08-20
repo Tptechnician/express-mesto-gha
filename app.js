4cf0a2162ab41fb27d916f9120b213a27b75e880
Pavel Demin <deminps.2008@gmail.com>
2022-08-20 20:19:04 +0500

    setting Prettier

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-const { application } = require("express");
-const express = require("express");
+const express = require('express');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.get("/", (req, res) =>{
-  res.send("Проверка")
+app.get('/', (req, res) => {
+  res.send('Проверка');
 });
 
 app.listen(PORT, () => {
