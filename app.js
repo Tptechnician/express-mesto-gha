212b59d830139f6a4e4658c8ac7779295798d3c4
Pavel Demin <deminps.2008@gmail.com>
2022-08-21 13:20:01 +0500

    connection mongodb

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 
@@ -8,6 +9,19 @@ app.get('/', (req, res) => {
   res.send('Проверка еще раз');
 });
 
-app.listen(PORT, () => {
-  console.log(`Сервер запущен на localhost:${PORT}`);
+app.post('/', express.json(), (req, res) => {
+  res.send(req.body);
 });
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/mestodb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: false,
+  });
+
+  await app.listen(PORT, () => {
+    console.log(`Сервер запущен на localhost:${PORT}`);
+  });
+}
+
+main();
