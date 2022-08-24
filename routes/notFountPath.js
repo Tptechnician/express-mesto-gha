a0e7521ce864bf8ef53420c79c58d886f8db6eed
Pavel Demin <deminps.2008@gmail.com>
2022-08-24 14:40:14 +0500

    error notFountPath message

--- routes/notFountPath.js
+++ routes/notFountPath.js
@@ -3,7 +3,7 @@ const express = require('express');
 const notFountPath = express.Router();
 
 notFountPath.all('*', (req, res) => {
-  res.send({ message: 'Запрашиваемого ресурса не существует' });
+  res.status(404).send({ message: 'Запрашиваемого ресурса не существует' });
 });
 
 module.exports = notFountPath;
