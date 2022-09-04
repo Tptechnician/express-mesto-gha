e980694f209585097f8d2827907193dc00ae0d71
Pavel Demin <deminps.2008@gmail.com>
2022-09-04 19:59:45 +0500

    validation Route user

--- routes/users.js
+++ routes/users.js
@@ -17,13 +17,23 @@ usersRouter.get('/users/me', getUser);
 
 usersRouter.get('/users/:userId', celebrate({
   params: Joi.object().keys({
-    id: Joi.string().required().hex().min(24)
+    userId: Joi.string().required().hex().min(24)
       .max(24),
   }),
 }), getUserId);
 
-usersRouter.patch('/users/me', updateUser);
+usersRouter.patch('/users/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateUser);
 
-usersRouter.patch('/users/me/avatar', updateAvatar);
+usersRouter.patch('/users/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required()
+      .regex(/^(https?:\/\/)?([\da-z.-]+).([a-z.]{2,6})([/\w.-]*)*\/?$/),
+  }),
+}), updateAvatar);
 
 module.exports = usersRouter;
